4e579f313b91a9b31fb00635c5168d8aacefc578
Noskov Artem <nowasmawesome@gmail.com>
2023-02-12 15:38:22 +0500

    Add enemies hp

--- Scenes/Enemies/TankT1.gd
+++ Scenes/Enemies/TankT1.gd
@@ -2,11 +2,32 @@ extends PathFollow2D
 
 
 var speed = 150
+var hp = 50
+
+onready var health_bar = get_node("HealthBar")
+
+
+func destroy():
+	self.queue_free()
+
+
+func on_hit(damage):
+	hp -= damage
+	health_bar.value = hp
+	if hp <= 0:
+		destroy()
 
 
 func move(delta):
 	set_offset(get_offset() + speed * delta)
+	health_bar.set_position(position - Vector2(24, 28))
 
 
 func _physics_process(delta):
 	move(delta)
+
+
+func _ready():
+	health_bar.max_value = hp
+	health_bar.value = hp
+	health_bar.set_as_toplevel(true)
--- Scenes/Turrets/Turret.gd
+++ Scenes/Turrets/Turret.gd
@@ -1,15 +1,24 @@
 extends Node2D
 
 
+var data
 var enemies = []
 var enemy = null
 var type
 var built = false
+var fire_ready = true
 
 
 func select_enemy():
 	if not enemy or not enemies.has(enemy):
 		enemy = enemies[0]
+
+
+func fire():
+	fire_ready = false
+	enemy.on_hit(data["damage"])
+	yield(get_tree().create_timer(data["rof"]), "timeout")
+	fire_ready = true
 
 
 func turn():
@@ -18,7 +27,8 @@ func turn():
 
 func _ready():
 	if built:
-		self.get_node("Range/CollisionShape2D").get_shape().radius = 0.5 * GameData.towers[type]["range"]
+		data = GameData.towers[type]
+		self.get_node("Range/CollisionShape2D").get_shape().radius = 0.5 * data["range"]
 
 
 func _physics_process(delta):
@@ -27,6 +37,9 @@ func _physics_process(delta):
 	else:
 		select_enemy()
 		turn()
+
+		if fire_ready:
+			fire()
 
 
 func _on_Range_body_entered(body):
